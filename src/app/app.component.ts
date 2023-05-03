0612be36560c59dc49ecae4dda894b4b27aa9e46
unknown <rimna.mk@gmail.com>
2023-05-03 16:24:44 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bank';
+  data="your perfect banking partner"
+  placeholderData="enter username"
+  placedata2="enter password"
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+  login(){
+
+    alert("login clicked")
+  }
+  unameChange(event:any){
+    console.log(event.target.value);
+    
+  }
 }
